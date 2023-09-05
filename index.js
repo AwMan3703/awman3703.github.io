9ff758f6c7317e882bcd43849db528bd955e8d7e
Aw Man <brambilladaniele@icloud.com>
2023-09-05 18:49:57 +0200

    general formatting

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const getId = () => { ids_counter += 1; return ids_counter; }
 const make = (tag) => { return document.createElement(tag); }
 const getExt = (path) => { return path.split('.').pop(); }
 
-
 //generic functions
 const playAudio = (path) => {
     const audio = new Audio(path);
@@ -56,7 +55,6 @@ const makePopup = (title, description, closeCallback = function(e){}) => {
     return popupNode;
 }
 
-
 //specific functions
 function btnHoverSound() {
     playAudio(`audio/menu-hover3.wav`)
@@ -77,6 +75,7 @@ window.onload = function() {
         }
         ))
 };
+
 
 
 
