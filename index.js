be2520a0bb62b5f2e9d9d349cbe942360ed8d40a
Aw Man <brambilladaniele@icloud.com>
2023-09-06 13:21:17 +0200

    menu completion
    menu section is now completed

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ const makePopup = (title, description, closeCallback = function(_){}) => {
     xNode.classList = 'pointer secondary-text'
     xNode.style.fontFamily = '\'Courier New\', Courier, monospace;'
     xNode.style.fontSize = 'xxx-large'
+    xNode.setAttribute('onmousedown', 'this.style.fontSize = \"10rem\"')
+    xNode.setAttribute('onmouseover', 'this.style.fontSize = \"4rem\"')
+    xNode.setAttribute('onmouseout', 'this.style.fontSize = \"xxx-large"')
     xNode.onclick = closePopup
     xNode.innerText = 'X'
 
