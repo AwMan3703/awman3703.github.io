db947494e0097854b7b875163eafcd76b5650eac
Aw Man <brambilladaniele@icloud.com>
2023-09-08 14:53:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -62,14 +62,12 @@ function makePopup(title, description, closeCallback = function(_){}) {
 //specific functions
 function startupPopup() {
     document.getElementById('popupblur').appendChild(makePopup(
-        /*'Work in progress :)',
+        'Work in progress :)',
         'This site is not yet finished, and may not work properly.'+
         '<br>(press/click X to dismiss)'+
         '<hr style="opacity:2%">'+
         '<span style="opacity:15%">For more information, contact me on Discord - Aw Man#3646'+
-        '<br>and Twitter (or X, whatever you wanna call it) - @Aw_Man3704</span>',*/
-        'Balls',
-        '\"Scrotum\"',
+        '<br>and Twitter (or X, whatever you wanna call it) - @Aw_Man3704</span>',
         function(_) {
             const e = document.getElementById('popupblur')
             e.style.opacity = '0%';
