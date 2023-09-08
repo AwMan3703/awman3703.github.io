048936774612d6fab39bedc3fd354274b80a0d53
Aw Man <brambilladaniele@icloud.com>
2023-09-08 13:51:40 +0200

    Update mobile-discrimination.js

--- mobile-discrimination.js
+++ mobile-discrimination.js
@@ -3,13 +3,13 @@
 function check() {
     const w = visualViewport.width
     const h = visualViewport.height
-    if (w < (h / 1.5)) { return true }
+    if (w < h) { return true }
     else { return false }
 }
 
 function discriminate() {
     if (check()) {
-        document.body.innerHTML = ''+
+        document.body.innerHTML = '<p style="font-size: 10vh;">'+
         '<h1>Imagine trying to view this on mobile</h1>'+
         '<h3>Cringe</h3>'+
         ''+
@@ -19,4 +19,5 @@ function discriminate() {
 
 window.addEventListener("resize", () => {
     discriminate()
+    console.log("mobile discrimination complete")
 })
