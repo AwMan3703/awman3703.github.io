bae8f8cd09fc9dfe4f120bdaa958e8395668c036
Aw Man <brambilladaniele@icloud.com>
2023-09-30 10:10:01 +0200

    started work on about card

--- index.js
+++ index.js
@@ -65,15 +65,15 @@ function makePopup(title, description, closeCallback = function(_){}) {
 
 //specific functions
 function startupPopup() {
-    document.getElementById('popupblur').appendChild(makePopup(
+    document.getElementById('popup-blur').appendChild(makePopup(
         'Work in progress :)',
         'This site is not yet finished, and may not work properly!'+
         '<br>(press/click X to dismiss)'+
         '<hr style="opacity:2%">'+
         '<span style="opacity:15%">For more information, contact me on Discord - Aw Man#3646'+
-        '<br>and Twitter (or X, whatever you wanna call it) - @Aw_Man3704</span>',
+        '<br>and Twitter (or X, whatever you want to call it) - @Aw_Man3704</span>',
         function(_) {
-            const e = document.getElementById('popupblur')
+            const e = document.getElementById('popup-blur')
             e.style.opacity = '0%';
             e.remove()
         }
