86bf8321cd25bef4990b757d1ed939c32f8ee841
Aw Man <brambilladaniele@icloud.com>
2023-10-10 22:32:06 +0200

    created pages index

--- portfolio/javascript/mobile-discrimination.js
+++ portfolio/javascript/mobile-discrimination.js
@@ -9,7 +9,7 @@ function check() {
 
 function discriminate() {
     if (check()) {
-        document.body.innerHTML = '<p css="font-size: 10vh;">'+
+        document.body.innerHTML = '<p _css="font-size: 10vh;">'+
         '<h1>Imagine trying to view this on mobile</h1>'+
         '<h3>Cringe</h3>'+
         '</p>'+
